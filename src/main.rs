b30e4f918db96249c3e3fca0cfaf4cf0566d0278
tkntkn <kd1049@gmail.com>
2022-09-19 16:09:50 +0900

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,47 @@
-use async_trait::async_trait;
-use futures_util::stream::SplitSink;
-use futures_util::{SinkExt, StreamExt};
 use std::fs::File;
 use std::io::{self, BufRead};
-use std::net::UdpSocket;
+use std::net::{TcpListener, TcpStream, UdpSocket};
 use std::path::Path;
+use std::sync::mpsc::Receiver;
 use std::sync::{mpsc, Arc, Mutex};
 use std::time::{SystemTime, UNIX_EPOCH};
 use std::{env, thread};
-use tokio::net::{TcpListener, TcpStream};
-use tokio_tungstenite::tungstenite::Message;
-use tokio_tungstenite::{accept_async, WebSocketStream};
+use tungstenite::{accept, Message, WebSocket};
 
-#[async_trait]
 trait Broker {
-    async fn broadcast(&self, message: &String);
+    fn broadcast(&self, message: String);
 }
 
 struct WebSocketBorker {
-    ws_streams: Arc<Mutex<Vec<SplitSink<WebSocketStream<TcpStream>, Message>>>>,
+    sockets: Arc<Mutex<Vec<WebSocket<TcpStream>>>>,
 }
 
 impl WebSocketBorker {
     fn new(addr: String) -> WebSocketBorker {
-        let broker = WebSocketBorker {
-            ws_streams: Arc::new(Mutex::new(Vec::new())),
-        };
-        let thread_ws_streams = broker.ws_streams.clone();
-        tokio::spawn(async move {
-            let listener = TcpListener::bind(addr).await.expect("Can't listen");
-            while let Ok((stream, _)) = listener.accept().await {
-                let ws_stream = accept_async(stream).await.expect("Failed to accept");
-                let (ws_sender, _) = ws_stream.split();
-                thread_ws_streams.lock().unwrap().push(ws_sender);
+        let sockets = Arc::new(Mutex::new(Vec::new()));
+
+        let server = TcpListener::bind(addr).unwrap();
+        let sockets_ref = sockets.clone();
+        thread::spawn(move || {
+            for stream in server.incoming() {
+                let socket = accept(stream.unwrap()).unwrap();
+                sockets_ref.lock().unwrap().push(socket);
             }
         });
-        broker
+
+        WebSocketBorker { sockets }
     }
 }
 
-#[async_trait]
 impl Broker for WebSocketBorker {
-    async fn broadcast(&self, message: &String) {
-        let thread_ws_streams = self.ws_streams.clone();
-        let thread_ws_streams_iter = thread_ws_streams.lock().unwrap().iter_mut();
-        let send_futures =
-            thread_ws_streams_iter.map(|ws_stream| ws_stream.send(Message::text(message)));
-        for future in send_futures {
-            future.await;
-        }
+    fn broadcast(&self, message: String) {
+        let sockets_ref = self.sockets.clone();
+        thread::spawn(move || {
+            let mut sockets = sockets_ref.lock().unwrap();
+            for socket in sockets.iter_mut() {
+                socket.write_message(Message::text(&message)).unwrap();
+            }
+        });
     }
 }
 
@@ -66,17 +59,59 @@ impl UdpBroker {
     }
 }
 
-#[async_trait]
 impl Broker for UdpBroker {
-    async fn broadcast(&self, message: &String) {
+    fn broadcast(&self, message: String) {
         self.socket
             .send_to(message.as_bytes(), &self.target_addr)
             .expect("failed to send message");
     }
 }
 
-#[tokio::main]
-async fn main() {
+trait FloorMessanger {
+    fn create_receiver(&self) -> Receiver<String>;
+}
+
+struct FileFloorMessanger {
+    filename: String,
+}
+
+impl FileFloorMessanger {
+    fn new(filename: String) -> FileFloorMessanger {
+        FileFloorMessanger { filename }
+    }
+}
+
+impl FloorMessanger for FileFloorMessanger {
+    fn create_receiver(&self) -> Receiver<String> {
+        let (tx, rx) = mpsc::channel();
+        let lines = read_lines(&self.filename);
+        thread::spawn(move || {
+            let start_epoch = current_epoch();
+            if let Ok(mut lines) = lines {
+                let first_line = lines.next().unwrap().expect("File is not empty");
+                let ShallowParsedData(data_start_epoch, _data) = shallow_parse_data(first_line);
+
+                for line in lines {
+                    if let Ok(line) = line {
+                        let ShallowParsedData(data_epoch, data) = shallow_parse_data(line);
+                        let data_time = data_epoch - data_start_epoch;
+                        loop {
+                            let past_time = current_epoch() - start_epoch;
+                            if past_time >= data_time {
+                                break;
+                            }
+                        }
+                        println!("{}", data_time);
+                        tx.send(data).unwrap();
+                    }
+                }
+            }
+        });
+        rx
+    }
+}
+
+fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 3 {
@@ -92,33 +127,10 @@ async fn main() {
         Box::new(UdpBroker::new(target_addr)),
     ];
 
-    let (tx, rx) = mpsc::channel();
-    thread::spawn(move || {
-        let start_epoch = current_epoch();
-        if let Ok(mut lines) = read_lines(filename) {
-            let first_line = lines.next().unwrap().expect("File is not empty");
-            let ShallowParsedData(data_start_epoch, _data) = shallow_parse_data(first_line);
-
-            for line in lines {
-                if let Ok(line) = line {
-                    let ShallowParsedData(data_epoch, data) = shallow_parse_data(line);
-                    let data_time = data_epoch - data_start_epoch;
-                    loop {
-                        let past_time = current_epoch() - start_epoch;
-                        if past_time >= data_time {
-                            break;
-                        }
-                    }
-                    println!("{}", data_time);
-                    tx.send(data).unwrap();
-                }
-            }
-        }
-    });
-
-    for data in rx {
+    let messanger = FileFloorMessanger::new(filename);
+    for data in messanger.create_receiver() {
         for broker in &brokers {
-            broker.broadcast(&data).await;
+            broker.broadcast(String::from(&data));
         }
     }
 }
