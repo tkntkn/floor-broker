097462e901916ceb5ca0f0a6b645ee6ff4c1c4d0
tkntkn <kd1049@gmail.com>
2022-10-07 00:28:59 +0900

    Enumの利用とRead+Writeの実装による重複の排除

--- src/broker.rs
+++ src/broker.rs
@@ -1,10 +1,9 @@
 use native_tls::{Identity, TlsAcceptor, TlsStream};
-// use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod, SslStream};
 use std::fs::File;
 use std::io::ErrorKind::{ConnectionAborted, WouldBlock};
-use std::io::{Read, Write};
+use std::io::{self, Read, Write};
 use std::net::{TcpListener, TcpStream, UdpSocket};
-use std::sync::{Arc, Mutex, MutexGuard};
+use std::sync::{Arc, Mutex};
 use std::thread;
 use tungstenite::error::Error::Io;
 use tungstenite::{accept, Message, WebSocket};
@@ -13,17 +12,101 @@ pub trait Broker {
     fn broadcast(&self, message: String);
 }
 
-pub trait WebSocketBroker<Stream: Read + Write> {
-    fn get_locked_sockets(&self) -> MutexGuard<'_, Vec<WebSocket<Stream>>>;
-    fn get_identifier(&self, socket: &Stream) -> String;
+enum FlexStream {
+    Tcp(TcpStream),
+    Tls(TlsStream<TcpStream>),
 }
 
-impl<Stream: Read + Write, T: WebSocketBroker<Stream>> Broker for T {
+impl FlexStream {
+    fn get_tcp_ref(&self) -> &TcpStream {
+        match self {
+            FlexStream::Tcp(s) => s,
+            FlexStream::Tls(s) => s.get_ref(),
+        }
+    }
+
+    fn set_nonblocking(&self, nonblocking: bool) -> io::Result<()> {
+        self.get_tcp_ref().set_nonblocking(nonblocking)
+    }
+
+    fn get_identifier(&self) -> String {
+        self.get_tcp_ref().peer_addr().unwrap().to_string()
+    }
+}
+
+impl Read for FlexStream {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        match self {
+            FlexStream::Tcp(s) => s.read(buf),
+            FlexStream::Tls(s) => s.read(buf),
+        }
+    }
+}
+
+impl Write for FlexStream {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        match self {
+            FlexStream::Tcp(s) => s.write(buf),
+            FlexStream::Tls(s) => s.write(buf),
+        }
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        match self {
+            FlexStream::Tcp(s) => s.flush(),
+            FlexStream::Tls(s) => s.flush(),
+        }
+    }
+}
+
+pub struct WebSocketBroker {
+    sockets: Arc<Mutex<Vec<WebSocket<FlexStream>>>>,
+}
+
+impl WebSocketBroker {
+    pub fn new(addr: String, secure_addr: String) -> WebSocketBroker {
+        let sockets = Arc::new(Mutex::new(Vec::new()));
+
+        let sockets_ref = sockets.clone();
+        let server = TcpListener::bind(addr).unwrap();
+        thread::spawn(move || {
+            for stream in server.incoming() {
+                let stream = stream.unwrap();
+                let socket = accept(FlexStream::Tcp(stream)).unwrap();
+                socket.get_ref().set_nonblocking(true).unwrap();
+                println!("Connected: {}.", socket.get_ref().get_identifier());
+                sockets_ref.lock().unwrap().push(socket);
+            }
+        });
+
+        let mut file = File::open("./cert/server.pfx").unwrap();
+        let mut identity = vec![];
+        file.read_to_end(&mut identity).unwrap();
+        let identity = Identity::from_pkcs12(&identity, "").unwrap();
+        let acceptor = TlsAcceptor::new(identity).unwrap();
+
+        let sockets_ref = sockets.clone();
+        let server = TcpListener::bind(secure_addr).unwrap();
+        thread::spawn(move || {
+            for stream in server.incoming() {
+                let stream = stream.unwrap();
+                let socket = accept(FlexStream::Tls(acceptor.accept(stream).unwrap())).unwrap();
+                socket.get_ref().set_nonblocking(true).unwrap();
+                println!("Connected: {}.", socket.get_ref().get_identifier());
+                sockets_ref.lock().unwrap().push(socket);
+            }
+        });
+
+        WebSocketBroker { sockets }
+    }
+}
+
+impl Broker for WebSocketBroker {
     fn broadcast(&self, message: String) {
-        self.get_locked_sockets().retain_mut(|socket| {
+        self.sockets.lock().unwrap().retain_mut(|socket| {
             match socket.read_message() {
                 Ok(message) if message.is_close() => {
-                    println!("Socket closed: {}.", self.get_identifier(socket.get_ref()));
+                    println!("Socket closed: {}.", socket.get_ref().get_identifier());
                     return false;
                 }
                 Ok(message) => panic!("[003] unknown message: {message}"),
@@ -33,90 +116,13 @@ impl<Stream: Read + Write, T: WebSocketBroker<Stream>> Broker for T {
             match socket.write_message(Message::text(&message)) {
                 Ok(()) => true,
                 Err(Io(e)) if e.kind() == ConnectionAborted => {
-                    println!(
-                        "Connection aborted: {}.",
-                        self.get_identifier(socket.get_ref())
-                    );
+                    println!("Connection aborted: {}.", socket.get_ref().get_identifier());
                     return false;
                 }
                 Err(e) => panic!("[002] encountered unknown error: {e}"),
             };
             socket.can_write()
         })
-    }
-}
-
-pub struct TcpWebSocketBroker {
-    sockets: Arc<Mutex<Vec<WebSocket<TcpStream>>>>,
-}
-
-impl TcpWebSocketBroker {
-    pub fn new(addr: String) -> TcpWebSocketBroker {
-        let sockets = Arc::new(Mutex::new(Vec::new()));
-        let sockets_ref = sockets.clone();
-
-        let server = TcpListener::bind(addr).unwrap();
-        thread::spawn(move || {
-            for stream in server.incoming() {
-                let socket = accept(stream.unwrap()).unwrap();
-                socket.get_ref().set_nonblocking(true).unwrap();
-                println!("Connected: {}.", socket.get_ref().peer_addr().unwrap());
-                sockets_ref.lock().unwrap().push(socket);
-            }
-        });
-
-        TcpWebSocketBroker { sockets }
-    }
-}
-
-impl WebSocketBroker<TcpStream> for TcpWebSocketBroker {
-    fn get_locked_sockets(&self) -> MutexGuard<'_, Vec<WebSocket<TcpStream>>> {
-        self.sockets.lock().unwrap()
-    }
-
-    fn get_identifier(&self, socket: &TcpStream) -> String {
-        socket.peer_addr().unwrap().to_string()
-    }
-}
-
-pub struct WebSocketSecureBroker {
-    sockets: Arc<Mutex<Vec<WebSocket<TlsStream<TcpStream>>>>>,
-}
-
-impl WebSocketSecureBroker {
-    pub fn new(addr: String) -> WebSocketSecureBroker {
-        let mut file = File::open("./cert/server.pfx").unwrap();
-        let mut identity = vec![];
-        file.read_to_end(&mut identity).unwrap();
-        let identity = Identity::from_pkcs12(&identity, "").unwrap();
-
-        let acceptor = TlsAcceptor::new(identity).unwrap();
-        let acceptor = Arc::new(acceptor);
-        let acceptor_ref = acceptor.clone();
-
-        let sockets = Arc::new(Mutex::new(Vec::new()));
-        let sockets_ref = sockets.clone();
-
-        let server = TcpListener::bind(addr).unwrap();
-        thread::spawn(move || {
-            for stream in server.incoming() {
-                let socket = accept(acceptor_ref.accept(stream.unwrap()).unwrap()).unwrap();
-                socket.get_ref().get_ref().set_nonblocking(true).unwrap();
-                sockets_ref.lock().unwrap().push(socket);
-            }
-        });
-
-        WebSocketSecureBroker { sockets }
-    }
-}
-
-impl WebSocketBroker<TlsStream<TcpStream>> for WebSocketSecureBroker {
-    fn get_locked_sockets(&self) -> MutexGuard<'_, Vec<WebSocket<TlsStream<TcpStream>>>> {
-        self.sockets.lock().unwrap()
-    }
-
-    fn get_identifier(&self, socket: &TlsStream<TcpStream>) -> String {
-        socket.get_ref().peer_addr().unwrap().to_string()
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,24 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 4 {
-        println!("Usage: command (replay <dat file> | serial <config file>) <websocket addr> [...<udp addrs>]");
+        println!("Usage: command (replay <dat file> | serial <config file>) <ws addr> <wss addr> [...<udp addrs>]");
         return;
     }
 
     let mode = &args[1];
     let filename = &args[2];
-    let webscoket_addr = &args[3];
-    let upd_addrs = args[4..]
+    let ws_addr = &args[3];
+    let wss_addr = &args[4];
+    let upd_addrs = args[5..]
         .into_iter()
         .map(|e| e.to_string())
         .collect::<Vec<String>>();
 
     let brokers: Vec<Box<dyn Broker>> = vec![
-        Box::new(TcpWebSocketBroker::new(webscoket_addr.to_string())),
-        // Box::new(WebSocketSecureBroker::new(webscoket_addr.to_string())),
+        Box::new(WebSocketBroker::new(
+            ws_addr.to_string(),
+            wss_addr.to_string(),
+        )),
         Box::new(UdpBroker::new(upd_addrs)),
     ];
 
